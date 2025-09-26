023d4e6355c0d159175e9423f7b32ee59e19fe92
dantt1002 <dantt@avadagroup.com>
2025-09-26 17:50:08 +0700

    [wip] minimum size

---
+++ excercise/02_Arrays/sliding_window/MinimumSizeSubArraySum.java
@@ -0,0 +1,43 @@
+// Given an array of positive integers nums and a positive integer target,
+// return the minimal length of a subarray whose sum is greater than or equal to
+// target. If there is no such subarray, return 0 instead.
+
+// Example 1:
+
+// Input: target = 7, nums = [2,3,1,2,4,3]
+// Output: 2
+// Explanation: The subarray [4,3] has the minimal length under the problem
+// constraint.
+// Example 2:
+
+// Input: target = 4, nums = [1,4,4]
+// Output: 1
+// Example 3:
+
+// Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+// Output: 0
+
+// Constraints:
+
+// 1 <= target <= 109
+// 1 <= nums.length <= 105
+// 1 <= nums[i] <= 104
+
+
+public class Solution {
+    public int minimumSizeSubArraySum(int target, int[] nums) {
+        int minimumSize = nums.length;
+        int left = 0;
+        for (int i = left; right < nums.length; i++) {
+            int sum = nums[i];
+            int size = 1;
+            if(sum >= target) {
+                minimumSize = Math.min(minimumSize, size);
+                continue;
+            };
+            sum+=nums[i];
+            size++;
+        }
+        return minimumSize;
+    }
+}
